1867d883c127c34129198db52fa86f0b535cf528
Daysha Jones <daysha.vu@icloud.com>
2021-06-04 14:57:55 +0000

    Automatically backed up by Learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,8 +1,7 @@
 class CreateCharacters < ActiveRecord::Migration[5.1]
   def change
-    create_table :actors do |t|
-      t.string :first_name
-      t.string :last_name
-    end
+    create_table :characters do |t|
+      t.integer :actor_id
+      t.integer :show_id
   end
 end
